45a322771a4053ae3fbbfa436489770f92ef0e7e
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-01 17:04:44 +0100

    Corregidos errores en Secret_Entrance.cpp

--- Secret_Entrance.cpp
+++ Secret_Entrance.cpp
@@ -1,44 +1,61 @@
+//#include<Murcia>
 #include<iostream>
 #include<fstream>
-#include<stdlib.h>
+#include<cstdlib>
+#include<string>
 using namespace std;
 
 int main(){
     ifstream input;
     input.open("input.txt");
 
-    int password=0,num=50;
-    char dir;
-    string pos,cad;
-    while(getline(input,cad)) {
-        dir = cad[0];
-        for (unsigned int i=1; i<cad.size();i++){
-            pos[i] = cad[i];
-        }
-         int aux = stoi(pos);
-        switch (dir)
-        {
-        case 'R':
-
-            if ((num + aux) > 99){
-                num = ((num + aux)- 100);
-            }else{
-                num = num + aux;
+    if(input.is_open()){
+        int password=0,num=50;
+        char dir;
+        string pos,cad;
+        int cont = 0;
+        while(getline(input,cad)) { //Revisamos linea por linea el archivo de movimientos
+            if(cad.empty()){
+                continue; // si la línea está vacía avanzamos a la siguiente
             }
-            break;
-        case 'L':
-            if ((num - aux) < 0) {
-                num = (100 - (num-aux));
-            }else {
-                num = num-aux;
-            }
-            break;
-        }
+            dir = cad[0]; //obtenemos el sentido de giro 
+            pos = cad.substr(1); //obtenemos el número de movimientos 
+            int aux = stoi(pos)%100; // obtenemos el número de movimientos obviando giros completos
+            switch (dir) //clasificamos según la direcci´pn de giro
+            {
+            case 'R':
 
-        if (num == 0){
-            password++;
+                if ((num + aux) > 99){
+                    num = ((num + aux)- 100);
+                    cont++;
+                }else{
+                    num = num + aux;
+                    cont++;
+                }
+                break;
+            case 'L':
+                if ((num - aux) < 0) {
+                    num = num - aux +100;
+                    cont++;
+                }else {
+                    num = num-aux;
+                    cont++;
+                }
+                break;
+            }
+
+            if (num == 0 ){
+                password++; //si el dial apunta a cero, incrementamos password
+                cout<<"En la linea "<<cont+1<<" el dial de la caja fuerte apunta a 0."<<endl;
+                cout<<"El giro era de "<<aux<<" unidades."<<endl;
+            }
         }
+        cout<<password<<endl;
+        input.close(); //cerramos archivo input
+        return 0;
+    }else{
+        cout<<"No se pudo abrir el archivo."<<endl; //si se produce algun error en la apertura de input se muestra este mensaje
+        return -1;
     }
-    cout<<password<<endl;
 
 }
