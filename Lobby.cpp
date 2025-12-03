07c4a3497aca6ff60f756234f4cb7c9bdf908513
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-03 16:38:36 +0100

    Ahora si que si errores corregidos

--- Lobby.cpp
+++ Lobby.cpp
@@ -8,8 +8,9 @@
 using namespace std;
 
 int maximize(const string cad, int i, int j, vector<vector<int>> &mem ){
+    
     if (i >= cad.length() || j >= cad.length() || i >= j) return 0;
-    else if (mem[i][j] != 0) mem[i][j];
+    else if (mem[i][j] != 0) return mem[i][j];
     else {
         string aux = {cad[i],cad[j]};
         mem[i][j] = max(max( stoi(aux) , maximize(cad,i+1,j,mem) ) , max(maximize(cad,i,j+1,mem),maximize(cad,i+1,j+1,mem)));
@@ -22,8 +23,8 @@ int main(){
     string cad;
     int sum=0;
     while(getline(input,cad)){
-        vector<vector<int>>(cad.size(),vector<int>(cad.size(),0)); //inicialización de la matriz de memoria a 0
-        sum+= maximize(cad,0,1,mem);
+        vector<vector<int>> mem(cad.size(),vector<int>(cad.size(),0));
+        sum += maximize(cad,0, 1,mem);
     }
     cout<<sum<<endl;
     return 0;
