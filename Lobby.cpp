d7e7690db4144c49415b72d38783ab40a8f43e78
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-04 18:45:54 +0100

    Código comentado

--- Lobby.cpp
+++ Lobby.cpp
@@ -9,10 +9,11 @@ using namespace std;
 
 int maximize(const string cad, int i, int j, vector<vector<int>> &mem ){
     
-    if (i >= cad.length() || j >= cad.length() || i >= j) return 0;
-    else if (mem[i][j] != 0) return mem[i][j];
+    if (i >= cad.length() || j >= cad.length() || i >= j) return 0; // Si i o j están out_of_range o si i > j = return 0
+    else if (mem[i][j] != 0) return mem[i][j];                      // Si ya existe el valor en la posición se devuelve directamente
     else {
         string aux = {cad[i],cad[j]};
+        // Se busca el máximo entre: (opción actual) - (avanzar i) - (avanzar j) - (avanzar ambas)
         mem[i][j] = max(max( stoi(aux) , maximize(cad,i+1,j,mem) ) , max(maximize(cad,i,j+1,mem),maximize(cad,i+1,j+1,mem)));
     }
     return mem[i][j];
@@ -23,8 +24,9 @@ int main(){
     string cad;
     int sum=0;
     while(getline(input,cad)){
+        // Creamos una matriz de tamaño cadxcad inicializada a 0
         vector<vector<int>> mem(cad.size(),vector<int>(cad.size(),0));
-        sum += maximize(cad,0, 1,mem);
+        sum += maximize(cad,0, 1,mem); //Sumamos los resultados
     }
     cout<<sum<<endl;
     return 0;
