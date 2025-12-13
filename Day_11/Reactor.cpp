30b80045211070a5a66ae6c39eb862eba3552c9f
include-Murcia <AoCincludemurcia@gmail.com>
2025-12-13 15:45:49 +0100

    Problema dia 11 parteuno hecho

--- Day_11/Reactor.cpp
+++ Day_11/Reactor.cpp
@@ -1,4 +1,3 @@
-//#include<Murica>
 #include<iostream>
 #include<fstream>
 #include<sstream>
@@ -6,8 +5,10 @@
 #include<stdexcept>
 #include "../HashTable/HashTable.h"
 #include "../Grafo/Graph.h"
+
+//#define DEBUG
 using namespace std;
-HashTable<int> tabla = HashTable<int>(30);
+HashTable<int> tabla = HashTable<int>(700);
 Graph grafo;
 void load(vector<vector<string>> &out){
     ifstream input("input.txt");
@@ -17,22 +18,19 @@ void load(vector<vector<string>> &out){
         vector<string> v_aux;                               //Creamos un vector auxiliar
         stringstream ss(line);                              //Creamos un string stream para leer la linea
         while(ss>>str){ 
-           str = (str.size() > 3) ? str.substr(0,3) : str; //Si la cadena tiene : se lo quitamos
-            v_aux.push_back(str);                          // Lo añadimos al vector para luego hacer la unión de nodos
-            try { //Miras si existe la clave, si existe saltas al bloque catch.
-                tabla.search(str);
-            }
-            catch (runtime_error &ex){
-                tabla.insert(str,cont); //Si no existía el string, insertas en la hash y en el grafo con valor = id = cont
-                grafo.insertNode(cont);
-                cont++;                 //Cont + 1
-            }
-        
+           if(str.size() > 3){
+            str = str.substr(0,3);
+            grafo.insertNode(cont);
+            tabla.insert(str,cont);
+            cont++;
+           }
+            v_aux.push_back(str);
         }               
-            
+        
         out.push_back(v_aux);
     }
-    
+    grafo.insertNode(cont);
+    tabla.insert("out",cont);
 }
 
 void add_neig(const vector<vector<string>> &adj){ //Funcion que une los nodos dada la lista de adyacencia
@@ -44,18 +42,34 @@ void add_neig(const vector<vector<string>> &adj){ //Funcion que une los nodos da
         }
     }
 }
+
+long long path_count(int id,vector<long long> &mem){
+    if( mem[id] != -1) return mem[id];
+    if (id == tabla.search("out")) return 1;
+    int cantidad_vecinos = grafo.graph[id].cantidad_vecinos();
+    int sum = 0;
+    for(int i=0; i < cantidad_vecinos;i++){
+        int new_id = grafo.graph[id].vecinos[i];
+        sum += path_count(new_id,mem);
+    }
+    mem[id] = sum;
+    return mem[id];
+
+}
 int main(){
     vector<vector<string>> input;
     load(input);
     add_neig(input);
 
+#ifdef DEBUG
     cout<<tabla<<endl<<endl;
     grafo.print(); cout<<endl;
+#endif
     //Búsqueda de los nodos inicio y final
     int id_you = tabla.search("you"); 
-    int id_out = tabla.search("out");
 
     //aplicación de algoritmo para saber cuantos caminos hay de un nodo a otro
-
+    vector<long long> mem(grafo.graph.size(),-1);
+    cout<<path_count(id_you,mem)<<endl;
     
 }
